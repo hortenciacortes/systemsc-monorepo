4107d8f020e6660cdd4a21bb195a8efc8c6ec9ec
Hortencia <hortencia.cortes@gmail.com>
2025-03-30 15:27:29 -0400

    ADD: libs button and storybook

--- apps/systemsc/src/app/app.component.ts
+++ apps/systemsc/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { ButtonComponent } from '@systemsc/button'
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, ButtonComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
