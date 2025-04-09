59d28930136e41c595714d26f38885f12ebed94f
Hortencia <hortencia.cortes@gmail.com>
2025-04-08 21:36:45 -0400

    ADD: input component

--- libs/input/src/lib/input/input.component.ts
+++ libs/input/src/lib/input/input.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -8,4 +8,19 @@ import { CommonModule } from '@angular/common';
   templateUrl: './input.component.html',
   styleUrl: './input.component.css',
 })
-export class InputComponent {}
+export class InputComponent {
+  @Input() multiline = false;
+  @Input() label = '';
+  @Input() id = '';
+  @Input() disabled = false;
+
+  getContainerClasses(): string {
+    let classes = 'input-container';
+
+    if (this.label) {
+      classes += ' with-label';
+    }
+
+    return classes;
+  }
+}
--- libs/storybook-host/src/lib/Molecules/input.component.stories.ts
+++ libs/storybook-host/src/lib/Molecules/input.component.stories.ts
@@ -8,5 +8,60 @@ export default meta;
 type Story = StoryObj<InputComponent>;
 
 export const Primary: Story = {
-  args: {},
+  args: {
+    multiline: false,
+  },
+};
+
+export const PrimaryDisabled: Story = {
+  args: {
+    ...Primary.args,
+    disabled: true,
+  },
+};
+
+export const Multiline: Story = {
+  args: {
+    ...Primary.args,
+    multiline: true,
+  },
+};
+
+export const MultilineDisabled: Story = {
+  args: {
+    ...Multiline.args,
+    disabled: true,
+  },
+};
+
+export const PrimaryWithLabel: Story = {
+  args: {
+    ...Primary.args,
+    label: 'Label',
+    id: 'primary-id',
+  },
+};
+
+export const PrimaryWithLabelDisabled: Story = {
+  args: {
+    ...PrimaryWithLabel.args,
+    id: 'primary-id-disabled',
+    disabled: true,
+  },
+};
+
+export const MultilineWithLabel: Story = {
+  args: {
+    ...Multiline.args,
+    label: 'Label',
+    id: 'multiline-id',
+  },
+};
+
+export const MultilineWithLabelDisabled: Story = {
+  args: {
+    ...MultilineWithLabel.args,
+    id: 'multiline-id-disabled',
+    disabled: true,
+  },
 };
